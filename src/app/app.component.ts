7422e4b1a36f27cc2ff2f9b115d17d3672822433
Avishek <choudhury.avishek1@tcs.com>
2019-05-20 00:52:46 +0530

    first draft of video chat app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,127 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AngularAgoraRtcService, Stream } from 'angular-agora-rtc';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnInit {
+  localStream: Stream;
+  remoteCalls: any  = [];
+  constructor(private agoraService: AngularAgoraRtcService) {
+    this.agoraService.createClient();
+  }
+ ngOnInit() {
+
+ }
+
+ /**
+  * method to start a call
+  */
+
+startCall() {
+  console.log('starting Call');
+  this.agoraService.client.join(null, '1000', null, (uid) => {
+    this.localStream =  this.agoraService.createStream(uid, true, null, null, true, false);
+    this.localStream.setVideoProfile('720p_3');
+    this.SubscribeToStreams();
+  });
+}
+/***
+ * endCall
+ */
+endCall() {
+const stream = this.agoraService.client.leave();
+console.log('disconnect stream', stream);
+this.agoraService.client.on('stream-removed', (evnt) => {
+  console.log('disconnected event', evnt);
+});
+// this.remoteCalls =  this.remoteCalls.filter(aCall =>  aCall !== `#agora_remote${stream.getId()}`);
+}
+/** subscribe to stream */
+private SubscribeToStreams() {
+this.localStream.on('accessAllowed', () => {
+console.log('AcessAllowed');
+});
+
+this.localStream.on('accessDenied', () => {
+  console.log('Access Denied');
+});
+
+this.localStream.init(() => {
+console.log('got userMedia successFull');
+this.localStream.play('agora_local');
+
+this.agoraService.client.publish(this.localStream, (err) => {
+console.log('Publish local stream err', err );
+});
+
+this.agoraService.client.on('strem-published', (evt) => {
+console.log('stream successFully Published');
+});
+}, (err) => {
+  console.log('getUserMedia failed, err');
+});
+
+// Another Pear id
+
+// for another pear id
+
+this.agoraService.client.on('error', (err) => {
+  console.log('error', err.reason);
+  if(err.reason === 'DYNAMIC_KEY_TIMEOUT') {
+    this.agoraService.client.renewChannelKey('', () => {
+      console.log('Renewed client id SuccessFully');
+    }, (error) => {
+      console.log('Renewed client id failed', error);
+    });
+
+  }
+});
+
+// subscribing the published stream
+
+this.agoraService.client.on('stream-added', (event) => {
+  const stream =  event.stream;
+  this.agoraService.client.subscribe(stream, (err) => {
+    console.log('Stream subscription failed', err);
+  });
+/** Push stream publisher Id on call */
+this.agoraService.client.on('stream-subscribed', (event) => {
+const stream  =  event.stream;
+if (!this.remoteCalls.includes(`agora_remote${stream.getId()}`)) {
+this.remoteCalls.push(`agora_remote${stream.getId()}`);
+setTimeout(() => stream.play(`agora_remote${stream.getId()}`), 2000);
+}
+});
+// ** On call cutt off */
+
+this.agoraService.client.on('stream-removed', (evt) => {
+  const stream  =  evt.stream;
+  stream.stop();
+  this.remoteCalls =  this.remoteCalls.filter(aCall =>  aCall !== `#agora_remote${stream.getId()}`);
+  console.log(`Remote stream is removed ${stream.getId()}`);
+
+});
+
+this.agoraService.client.on('peer-leave', (evt) => {
+  const stream =  evt.stream;
+  if (stream) {
+    stream.stop();
+    this.remoteCalls = this.remoteCalls.filter(aCall => aCall === `#agora_remote${stream.getId()}`);
+    console.log(`${evt.uid} left from this channel`);
+  }
+});
+});
+
+
+
+this.agoraService.client.on('stream-added', (event) => {
+const stream =  event.stream;
+
+})
+
+}
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AngularAgoraRtcModule, AgoraConfig } from 'angular-agora-rtc';
 
+const agoraConfig: AgoraConfig =  {
+  AppID: '079ebc7264974955b0689e8f307d40b6'
+
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularAgoraRtcModule.forRoot(agoraConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
